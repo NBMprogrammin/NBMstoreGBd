dd8fed2e8e59769afb567dd96b5f0e4ed943b7fa
noururstart25t <mnr20mhd20@gmail.com>
2025-12-13 23:59:54 +0300

    الرفع الأول: مشروع Laravel

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@ use App\Http\Controllers\FrountEndController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
 Route::post('/cod-email-register', [FrountEndController::class, 'sendcodtocreatenewaccounte']);
 
 Route::post('/cod-shange-email', [FrountEndController::class, 'starttoshangeemailprofile'])->middleware('auth:sanctum');
